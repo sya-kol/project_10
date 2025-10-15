25c21e1cae598192b095063cadc478ad3635b085
Kolesnikova Anastasia <nastya_kol@mail.ru>
2025-10-16 00:54:50 +0300

    add file README.md

--- src/processing.py
+++ src/processing.py
@@ -17,3 +17,13 @@ def sort_by_date(banking_operation: List[Dict], reverse: bool = True) -> List[Di
     задающий порядок сортировки (по умолчанию — убывание),
     возвращает новый список, отсортированный по дате"""
     return sorted(banking_operation, key=lambda x: x.get('date'), reverse=reverse)
+
+banking_operation = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+print(filter_by_state(banking_operation))
+print(sort_by_date(banking_operation))
