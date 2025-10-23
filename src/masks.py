a10576c43b982057e79189aa450ec019b34c3201
Kolesnikova Anastasia <nastya_kol@mail.ru>
2025-10-24 00:50:00 +0300

    add test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,21 @@
-def get_mask_card_number(number_cart: int) -> str:
+from curses.ascii import isdigit
+
+
+def get_mask_card_number(number_cart: str) -> str:
     """Функция принимает номер карты в виде числа и
     возвращает маску номера XXXX XX** **** XXXX"""
     number_cart_str = str(number_cart)
-    return f"{number_cart_str[:4]} {number_cart_str[4:6]}** **** {number_cart_str[-4:]}"
+    if len(number_cart_str) != 16 or not number_cart_str.isdigit():
+        raise ValueError("Не тот формат номера карты")
+    number_cart_mask = number_cart_str[:4] + " " + number_cart_str[4:6] + "**" + " " + "****" + " " + number_cart_str[-4:]
+    return number_cart_mask
 
 
-def get_mask_account(number_account: int) -> str:
+def get_mask_account(number_account: str) -> str:
     """Функция принимает номер счета в виде числа и
     возвращает маску номера **XXXX"""
     number_account_str = str(number_account)
-    return f"**{number_account_str[-4:]}"
+    if len(number_account_str) != 20 or not number_account_str.isdigit():
+        raise ValueError("Не тот формат номера карты")
+    number_account_mask = "**" + number_account_str[-4:]
+    return number_account_mask
---
+++ tests/test_masks.py
@@ -0,0 +1,41 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize('numbers, expected', [(7000792289606361, '7000 79** **** 6361'),
+                                              ('0000000000000000', '0000 00** **** 0000'),
+                                              ('0000000000009999', '0000 00** **** 9999'),
+                                              (8888888888888888, '8888 88** **** 8888')])
+def test_mask_card_number(numbers, expected):
+    assert get_mask_card_number(numbers) == expected
+
+
+@pytest.fixture
+def number_card():
+    return [70007922896063611, 700079228960636, '', 'eeeeeeeeeeeeeeee', '1212ffffffffffff']
+
+
+def test_mask_card_number_format():
+    with pytest.raises(ValueError):
+        get_mask_card_number(number_card)
+
+
+    # 73654108430135874305  # входной аргумент
+    # ** 4305  # выход функции
+
+@pytest.mark.parametrize('number_acc, expected', [(73654108430135874305, '**4305'),
+                                                  ('00000000000000000000', '**0000'),
+                                                  ('00000000000000009999', '**9999'),
+                                                  (88888888888888888888, '**8888')])
+def test_mask_account(number_acc, expected):
+    assert get_mask_account(number_acc) == expected
+
+
+@pytest.fixture
+def number_account():
+    return [700079220000896063611, 700079228960636, '', 'eeeeeeeeeeeeeeeeeeee', '11111111112131dgdfhd']
+
+
+def test_mask_account_format():
+    with pytest.raises(ValueError):
+        get_mask_account(number_account)
